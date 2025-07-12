a2cff7af10a70c99d20f87a4503350f17af607ef
shlokgupta16072005 <145221658+shlokgupta16072005@users.noreply.github.com>
2025-07-12 20:02:01 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = (int)(Math.random()*100 + 1);
+        int count = 0;
+                int guess = input.nextInt();
+        while(guess != number) {
+            if(guess < number) {
+                System.out.println("Your guess is too low. Try again.");
+                count++;
+                int nextGuess = input.nextInt();
+                guess = nextGuess;
+            } else if( guess == number) {
+                System.out.println("Congratulations! You guessed the number.");
+                break;
+            } else {
+                System.out.println("Your guess is too high. Try again.");
+                count++;
+                int nextGuess = input.nextInt();
+                guess = nextGuess;
+            }
+        }
+        System.out.println("You guessed the number in " + count + " attempts.");
+        input.close();
+    }
+}
